00cce5b7040ad9470b4f139cdc0efb7defe57fd2
github username <github email address>
2019-12-11 16:55:34 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+   
+    attr_accessor :roster, :name, :school
+
+    def initialize(school)
+        @roster = {}
+        @school = school
+    end
+
+    def add_student(name, grade)
+          if @roster[grade]     # roster is empty 
+             @roster[grade] << name        #add key with array as value, and student in array
+          else   #roster has at least one student
+            @roster[grade] = []
+            @roster[grade] << name       #push the student to the existing key
+          end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort 
+        hash = Hash.new
+        
+        @roster.each do |k, v|
+            hash[k] = v.sort 
+        end
+        hash
+    end
+    
+end
